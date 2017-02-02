9e634ab856035f10e5630124aef71f0a8bc4fa6c
terryzeng <terryzeng@Note3403>
2017-02-02 11:07:39 -0400

    first version

---
+++ method.py
@@ -0,0 +1,8 @@
+#Filename: method.py
+
+class Person:
+    def sayHi(self):
+        print 'Hello,how are you?'
+
+p = Person()
+p.sayHi()
---
+++ mymodule_demo.py
@@ -0,0 +1,6 @@
+#Filename: mymodule_demo.py
+
+import mymodule
+
+mymodule.sayhi()
+print 'Version',mymodule.version
