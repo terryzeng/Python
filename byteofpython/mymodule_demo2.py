78362ef6079f32b8413f0c56f984e2554982242e
terryzeng <terryzeng@Note3403>
2017-02-06 17:23:47 -0400

    recover the file

---
+++ byteofpython/mymodule_demo2.py
@@ -0,0 +1,8 @@
+#Filename: mymodule_demo2.py
+
+from mymodule import sayhi,version
+#Alternative:
+#from mymodule import
+
+sayhi()
+print 'Version',version
---
+++ byteofpython/using_sys.py
@@ -0,0 +1,9 @@
+#Filename: using_sys.py
+
+import sys
+
+print 'The command line arguments are:'
+for i in sys.argv:
+    print i
+
+print '\n\nThe PYTHONPATH is', sys.path, '\n'
