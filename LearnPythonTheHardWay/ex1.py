efdacd641a5478a55c353cb308fdffa8185c0c24
terry.zeng <terryzeng98@gmail.com>
2017-04-04 17:12:39 -0400

    add some exercise file to git server

---
+++ LearnPythonTheHardWay/ex1.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+print "我们的python!"
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "THis is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print ""
---
+++ LearnPythonTheHardWay/ex13_03.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, s1, s2 = argv
+
+input_1 = raw_input("Your " + s1 + " is:")
+input_2 = raw_input("Your " + s2 + " is:")
+
+print "Your input %s is: %s" % (s1, input_1)
+print "Your input %s is: %s" % (s2, input_2)
